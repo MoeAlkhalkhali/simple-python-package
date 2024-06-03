206435a371b2a64166e58e4b1f72931cd4484fd8
s123705 <moe.al-khalkhali@iag.com.au>
2024-06-03 14:13:59 +1000

    added simple class

---
+++ SimplePythonPackage/BusinessRule.py
@@ -0,0 +1,10 @@
+class BusinessRule:
+    def __init__(self):
+        print("Created Class")
+        
+    def predict(cost): 
+        if cost > 100:
+            return "cheap"
+        if cost > 90:
+            return "Very Cheap"
+        return "super cheap"
